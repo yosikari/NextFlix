4f3c6fbb15493e0db12448f234b6d8fe9f59f099
yosikari <yosikari@gmail.com>
2023-03-27 10:28:00 +0300

    upgrade guestMod

--- pages/login.tsx
+++ pages/login.tsx
@@ -34,7 +34,14 @@ function Login() {
     }
 
     const guestMod = async () => {
-        await signIn('guest@guest.com', 'guest123')
+        await signUp(`guest-${guidGenerator()}@guest.com`, 'guest123')
+    }
+
+    function guidGenerator() {
+        var S4 = function() {
+           return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        };
+        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
     }
 
     return (
