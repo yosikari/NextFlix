1b898ef6a5040128ca0842aa90035517b1d0aa2d
Yossi Karasik <yosikari@gmail.com>
2023-05-23 13:37:51 +0300

    google-site-verification

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@ const withTM = require('next-transpile-modules')([
   '@stripe/firestore-stripe-payments',
 ])
 
-/** @type {import('next').NextConfig} */
 module.exports = withTM({
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org', 'assets.nflxext.com']
   },
+  async rewrites() {
+    return [
+      {
+        source: '/google59fe515b0944de80.html',
+        destination: '/google59fe515b0944de80',
+      },
+    ];
+  },
 })
-
